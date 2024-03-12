bc22b55097f16fd2ad85cbcd9a349938bd082242
raoul <raoul.el-mir@insa-lyon.fr>
2024-03-12 11:47:07 +0100

    AffectConst incomplet

--- src/ir.h
+++ src/ir.h
@@ -45,4 +45,18 @@ protected:
     std::string rightVar;
 };
 
+class AffectConst : public IRInstr {
+    public: 
+        AffectConst(std::string left, std::string right)
+        : leftVar(left), rightVar(right){};
+    virtual void accept(IRBaseVisitor &visitor) override;
+    const std::string &getLeft() const { return leftVar; };
+    const std::string &getRight() const { return rightVar; };
+
+protected:
+    std::string leftVar;
+    std::string rightVar;
+
+};
+
 } // namespace ir
