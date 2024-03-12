627b5a478ea89af40da75d34a5a7390b965932ae
Alex4969 <alex.biaud@gmail.com>
2024-03-12 11:46:05 +0100

    IRx86 cpp

--- src/IRBaseVisitor.h
+++ src/IRBaseVisitor.h
@@ -6,4 +6,5 @@ public:
     virtual void visitAffect(ir::Affect &affect) = 0;
     virtual void visitAffectConst(ir::AffectConst &affectConst) = 0;
 
+    virtual void visitAdd(ir::Add &add) = 0;
 };
--- src/IRx86Visitor.cpp
+++ src/IRx86Visitor.cpp
@@ -1,15 +1,3 @@
 #include "IRx86Visitor.h"
 
-
-class IRx86Visitor : public IRBaseVisitor {
-    void visitAffect(ir::Affect &affect) {
-        std::cout << "    mov # A compléter !";
-    }
-
-    void visitAffectConst(ir::AffectConst &affectConst) {
-        std::cout << "    mov # A compléter !";
-
-
-    }
-
-};
+void IRx86Visitor::visitAffect(ir::Affect &affect) {}
