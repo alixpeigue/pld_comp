ee2e2fdca513f57758188e49c0eb563a170281c9
Alix Peigue <irobot.moi@gmail.com>
2024-03-13 16:03:27 +0100

    Basic operations working with IR

--- src/IRx86Visitor.cpp
+++ src/IRx86Visitor.cpp
@@ -1,3 +1,114 @@
 #include "IRx86Visitor.h"
+#include "Scope.h"
+#include "ir.h"
+#include <iostream>
 
-void IRx86Visitor::visitAffect(ir::Affect &affect) {}
+void IRx86Visitor::visitAffect(ir::Affect &affect) {
+    Scope &scope = affect.getBlock().getScope();
+    // scope.print(std::cout);
+    // std::cout << "TO : " << affect.getTo() << " FROM " << affect.getFrom()
+    //           << "\n";
+    Variable to = scope.getVariable(affect.getTo()).value();
+    Variable from = scope.getVariable(affect.getFrom()).value();
+    std::cout << "    mov eax, DWORD PTR -" << from.second << "[rbp]\n";
+    std::cout << "    mov DWORD PTR -" << to.second << "[rbp], eax\n";
+}
+
+void IRx86Visitor::visitAffectConst(ir::AffectConst &affectConst) {
+    Variable to = affectConst.getBlock()
+                      .getScope()
+                      .getVariable(affectConst.getTo())
+                      .value();
+    std::cout << "    mov DWORD PTR -" << to.second << "[rbp], "
+              << affectConst.getValue() << "\n";
+}
+
+void IRx86Visitor::visitBinOp(ir::BinOp &binop) {
+    Scope &scope = binop.getBlock().getScope();
+    Variable to = scope.getVariable(binop.getTo()).value();
+    Variable left = scope.getVariable(binop.getLeft()).value();
+    Variable right = scope.getVariable(binop.getRight()).value();
+
+    std::cout << "    mov eax, DWORD PTR -" << left.second << "[rbp]\n";
+
+    ir::BinOp::BinOpType type = binop.getType();
+
+    if (type == ir::BinOp::DIV || type == ir::BinOp::MOD) {
+        std::cout << "    cdq\n";
+        std::cout << "    idiv DWORD PTR -" << right.second << "[rbp]\n";
+        std::string reg = type == ir::BinOp::MOD ? "edx" : "eax";
+        std::cout << "    mov DWORD PTR -" << to.second << "[rbp], " << reg
+                  << "\n";
+        return;
+    }
+
+    std::string instr = "ERR";
+    switch (type) {
+    case ir::BinOp::ADD:
+        instr = "add";
+        break;
+    case ir::BinOp::SUB:
+        instr = "sub";
+        break;
+    case ir::BinOp::MUL:
+        instr = "imul";
+        break;
+    }
+
+    std::cout << "    " << instr << " eax, DWORD PTR -" << right.second
+              << "[rbp]\n";
+    std::cout << "    mov DWORD PTR -" << to.second << "[rbp], eax\n";
+}
+
+void IRx86Visitor::visitUnaryOp(ir::UnaryOp &unaryop) {
+    Scope &scope = unaryop.getBlock().getScope();
+    Variable to = scope.getVariable(unaryop.getTo()).value();
+    Variable from = scope.getVariable(unaryop.getFrom()).value();
+
+    std::cout << "    mov eax, DWORD PTR -" << from.second << "[rbp]\n";
+
+    switch (unaryop.getType()) {
+    case ir::UnaryOp::NEG:
+        std::cout << "neg eax\n";
+        break;
+    }
+
+    std::cout << "    mov DWORD PTR -" << to.second << "[rbp], eax\n";
+}
+
+void IRx86Visitor::visitBasicBlock(ir::BasicBlock &bb) {
+    std::cout << bb.getName() << ":\n";
+    for (auto &instruction : bb.getInstructions()) {
+        instruction->accept(*this);
+    }
+    // Call end block here :
+    bb.getNext().accept(*this);
+}
+
+void IRx86Visitor::visitCFG(ir::CFG &cfg) {
+
+    // prelude
+    std::cout << ".globl " << cfg.getName() << "\n";
+    std::cout << cfg.getName() << ":\n";
+    std::cout << "    push rbp\n";
+    std::cout << "    mov rbp, rsp\n";
+
+    // blocks
+    for (auto &block : cfg.getBlocks()) {
+        block->accept(*this);
+    }
+
+    // epilogue
+}
+
+void IRx86Visitor::visitUnconditionalJump(ir::UnconditionalJump &jump) {
+    std::cout << "    jmp " << jump.getTo().getName() << "\n";
+}
+
+void IRx86Visitor::visitReturn(ir::Return &ret) {
+    Variable retVar = ret.getBlock().getScope().getVariable("#return").value();
+
+    std::cout << "    mov eax, DWORD PTR -" << retVar.second << "[rbp]\n";
+    std::cout << "    pop rbp\n";
+    std::cout << "    ret\n";
+}
--- src/ir.h
+++ src/ir.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "Scope.h"
+#include <cstdint>
 #include <memory>
 #include <string>
 #include <vector>
@@ -8,55 +10,150 @@ class IRBaseVisitor;
 
 namespace ir {
 
+class BasicBlock;
+
 class IRInstr {
+    friend BasicBlock;
+
 public:
     virtual void accept(IRBaseVisitor &visitor) = 0;
-};
-
-class BasicBlock {
-public:
-    void addInstr(std::unique_ptr<IRInstr> instr);
-    void visit(IRBaseVisitor &visitor);
+    BasicBlock &getBlock() { return *block; };
 
 protected:
+    BasicBlock *block;
+};
+
+class Next : public IRInstr {};
+
+class CFG;
+
+class BasicBlock {
+    friend CFG;
+
+public:
+    BasicBlock(Scope *scope, std::string name)
+        : scope(scope), name(std::move(name)) {}
+    void addInstr(std::unique_ptr<IRInstr> instr);
+    void accept(IRBaseVisitor &visitor);
+    Scope &getScope() { return *scope; }
+    const std::string &getName() const { return name; }
+    const std::vector<std::unique_ptr<IRInstr>> &getInstructions() const {
+        return instrs;
+    }
+    Next &getNext() const { return *next; }
+    void setNext(std::unique_ptr<Next> next);
+    CFG &getCFG() { return *cfg; }
+
+protected:
+    std::string name;
     std::vector<std::unique_ptr<IRInstr>> instrs;
+    Scope *scope;
+    std::unique_ptr<Next> next;
+    CFG *cfg;
+};
+
+class UnconditionalJump : public Next {
+public:
+    UnconditionalJump(BasicBlock *to) : to(to) {}
+
+    virtual void accept(IRBaseVisitor &visitor) override;
+    void setTo(BasicBlock *block) { to = block; }
+    BasicBlock &getTo() { return *to; }
+
+protected:
+    BasicBlock *to;
+};
+
+class Return : public Next {
+public:
+    virtual void accept(IRBaseVisitor &visitor) override;
 };
 
 class CFG {
 public:
-    void addRoot(std::unique_ptr<BasicBlock> block);
+    CFG(std::string name) : name(std::move(name)) {}
+    void addBlock(std::unique_ptr<BasicBlock> block);
+    void addScope(std::unique_ptr<Scope> scope);
+    void setEpilogue(BasicBlock *epilogue) { this->epilogue = epilogue; }
+    BasicBlock &getEpilogue() { return *epilogue; }
 
-    void visitRoots(IRBaseVisitor &visitor);
+    void visitBlocks(IRBaseVisitor &visitor);
+    const std::string &getName() { return name; }
+    const std::vector<std::unique_ptr<BasicBlock>> &getBlocks() const {
+        return blocks;
+    }
+    // Scope *mcainScope;
 
 protected:
-    std::vector<std::unique_ptr<BasicBlock>> roots;
+    std::vector<std::unique_ptr<BasicBlock>> blocks;
+    std::vector<std::unique_ptr<Scope>> scopes;
+    BasicBlock *epilogue;
+    std::string name;
 };
+
+using Prog = std::vector<std::unique_ptr<CFG>>;
 
 class Affect : public IRInstr {
 public:
-    Affect(std::string left, std::string right)
-        : leftVar(left), rightVar(right){};
+    Affect(std::string to, std::string from)
+        : to(std::move(to)), from(std::move(from)){};
     virtual void accept(IRBaseVisitor &visitor) override;
-    const std::string &getLeft() const { return leftVar; };
-    const std::string &getRight() const { return rightVar; };
+    const std::string &getTo() const { return to; };
+    const std::string &getFrom() const { return from; };
 
 protected:
-    std::string leftVar;
-    std::string rightVar;
+    std::string to;
+    std::string from;
 };
 
 class AffectConst : public IRInstr {
-    public: 
-        AffectConst(std::string left, std::string right)
-        : leftVar(left), rightVar(right){};
+public:
+    AffectConst(std::string left, int value)
+        : to(std::move(left)), value(value){};
     virtual void accept(IRBaseVisitor &visitor) override;
-    const std::string &getLeft() const { return leftVar; };
-    const std::string &getRight() const { return rightVar; };
+    const std::string &getTo() const { return to; };
+    int getValue() const { return value; };
 
 protected:
-    std::string leftVar;
-    std::string rightVar;
+    std::string to;
+    int value;
+};
 
+class UnaryOp : public IRInstr {
+public:
+    enum UnaryOpType { NEG, NOT };
+
+    UnaryOp(UnaryOpType type, std::string to, std::string from)
+        : type(type), to(to), from(from) {}
+
+    const std::string &getTo() const { return to; }
+    const std::string &getFrom() const { return from; };
+    UnaryOpType getType() const { return type; }
+    virtual void accept(IRBaseVisitor &visitor) override;
+
+protected:
+    UnaryOpType type;
+    std::string to;
+    std::string from;
+};
+
+class BinOp : public IRInstr {
+public:
+    enum BinOpType { ADD, SUB, DIV, MUL, MOD };
+
+    BinOp(BinOpType type, std::string to, std::string from, std::string right)
+        : type(type), to(to), left(from), right(right) {}
+    const std::string &getTo() const { return to; }
+    const std::string &getLeft() const { return left; }
+    const std::string &getRight() const { return right; }
+    BinOpType getType() const { return type; }
+    virtual void accept(IRBaseVisitor &visitor) override;
+
+protected:
+    BinOpType type;
+    std::string to;
+    std::string left;
+    std::string right;
 };
 
 } // namespace ir
