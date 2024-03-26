b2bcfac6d3a38986d042902aa2f9cadc7e05d844
Alix Peigue <irobot.moi@gmail.com>
2024-03-26 08:42:13 +0100

    added empty class ConstantPropagationVisitor

---
+++ src/ConstantPropagationVisitor.cpp
@@ -0,0 +1,8 @@
+#include "ConstantPropagationVisitor.h"
+#include "ir.h"
+
+void ConstantPropagationVisitor::visitAffectConst(ir::AffectConst &affect) {}
+
+void ConstantPropagationVisitor::visitAffect(ir::Affect &affect){
+
+};
