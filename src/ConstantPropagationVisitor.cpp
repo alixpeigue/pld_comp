f8b819f39e9eaced09f424b6d986cfa19d0a1ef9
Amar <amarhasantawfiq@outlook.com>
2024-03-26 15:48:14 +0100

    Remove debug traces

--- src/ConstantPropagationVisitor.cpp
+++ src/ConstantPropagationVisitor.cpp
@@ -1,6 +1,5 @@
 #include "ConstantPropagationVisitor.h"
 #include "ir.h"
-#include <iostream>
 
 void ConstantPropagationVisitor::visitCFG(ir::CFG &cfg) {
     for (const auto &block : cfg.getBlocks()) {
@@ -13,21 +12,16 @@ void ConstantPropagationVisitor::visitCFG(ir::CFG &cfg) {
 }
 
 void ConstantPropagationVisitor::visitAffectConst(ir::AffectConst &affect) {
-    std::cerr << affect.toString() << std::endl;
     this->constants[affect.getTo()] = affect.getValue();
 }
 
 void ConstantPropagationVisitor::visitAffect(ir::Affect &affect) {
-    std::cerr <<"OLD: ";
-    std::cerr << affect.toString() << std::endl;
     if (this->constants.find(affect.getFrom()) != this->constants.end()) {
         int value = this->constants.at(affect.getFrom());
         this->constants[affect.getTo()] = value;
         ir::IRInstr *affectConst = new ir::AffectConst(affect.getTo(), value);
         affectConst->setBlock(&affect.getBlock());
         this->instr->reset(affectConst);
-    std::cerr <<"NEW: ";
-        std::cerr << affectConst->toString() << std::endl;
     }
 }
 
