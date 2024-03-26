4fcfe8524a2027e8ee96df7adaae0b08d49ba697
Amar <amarhasantawfiq@outlook.com>
2024-03-26 15:46:05 +0100

    Clear constants map after end of block

--- src/ConstantPropagationVisitor.cpp
+++ src/ConstantPropagationVisitor.cpp
@@ -1,5 +1,6 @@
 #include "ConstantPropagationVisitor.h"
 #include "ir.h"
+#include <iostream>
 
 void ConstantPropagationVisitor::visitCFG(ir::CFG &cfg) {
     for (const auto &block : cfg.getBlocks()) {
@@ -7,20 +8,26 @@ void ConstantPropagationVisitor::visitCFG(ir::CFG &cfg) {
             this->instr = &i;
             i->accept(*this);
         }
+        constants.clear();
     }
 }
 
 void ConstantPropagationVisitor::visitAffectConst(ir::AffectConst &affect) {
+    std::cerr << affect.toString() << std::endl;
     this->constants[affect.getTo()] = affect.getValue();
 }
 
 void ConstantPropagationVisitor::visitAffect(ir::Affect &affect) {
+    std::cerr <<"OLD: ";
+    std::cerr << affect.toString() << std::endl;
     if (this->constants.find(affect.getFrom()) != this->constants.end()) {
         int value = this->constants.at(affect.getFrom());
         this->constants[affect.getTo()] = value;
         ir::IRInstr *affectConst = new ir::AffectConst(affect.getTo(), value);
         affectConst->setBlock(&affect.getBlock());
         this->instr->reset(affectConst);
+    std::cerr <<"NEW: ";
+        std::cerr << affectConst->toString() << std::endl;
     }
 }
 
