70e830f89852f4222c5227a70e67ba115d1214bf
Amar <amarhasantawfiq@outlook.com>
2024-03-26 11:36:46 +0100

    Propagate unary and binary operation results

--- src/ConstantPropagationVisitor.cpp
+++ src/ConstantPropagationVisitor.cpp
@@ -24,3 +24,54 @@ void ConstantPropagationVisitor::visitAffect(ir::Affect &affect) {
         this->instr->reset(affectConst);
     }
 }
+
+void ConstantPropagationVisitor::visitUnaryOp(ir::UnaryOp &unaryOp) {
+    if (this->constants.find(unaryOp.getFrom()) != this->constants.end()) {
+        int value = this->constants.at(unaryOp.getFrom());
+
+        switch (unaryOp.getType()) {
+        case ir::UnaryOp::NEG:
+            value = -value;
+            break;
+        case ir::UnaryOp::NOT:
+            value = !value;
+            break;
+        }
+
+        this->constants.emplace(unaryOp.getTo(), value);
+        ir::IRInstr *unaryOpConst = new ir::AffectConst(unaryOp.getTo(), value);
+        unaryOpConst->setBlock(&unaryOp.getBlock());
+        this->instr->reset(unaryOpConst);
+    }
+}
+
+void ConstantPropagationVisitor::visitBinOp(ir::BinOp &binOp) {
+    if (this->constants.find(binOp.getLeft()) != this->constants.end() &&
+        this->constants.find(binOp.getRight()) != this->constants.end()) {
+        int value = this->constants.at(binOp.getLeft());
+        int rightValue = this->constants.at(binOp.getRight());
+
+        switch (binOp.getType()) {
+        case ir::BinOp::ADD:
+            value += rightValue;
+            break;
+        case ir::BinOp::SUB:
+            value -= rightValue;
+            break;
+        case ir::BinOp::MUL:
+            value *= rightValue;
+            break;
+        case ir::BinOp::DIV:
+            value /= rightValue;
+            break;
+        case ir::BinOp::MOD:
+            value %= rightValue;
+            break;
+        }
+
+        this->constants.emplace(binOp.getTo(), value);
+        ir::IRInstr *binOpConst = new ir::AffectConst(binOp.getTo(), value);
+        binOpConst->setBlock(&binOp.getBlock());
+        this->instr->reset(binOpConst);
+    }
+}
