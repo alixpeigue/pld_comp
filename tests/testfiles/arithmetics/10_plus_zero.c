b62e1a343e3db1974dcf042ae0a00a69da7ed85c
Amar <amarhasantawfiq@outlook.com>
2024-04-02 08:37:56 +0200

    Add neutral element support for constant propagation + add arithmetic tests

---
+++ tests/testfiles/arithmetics/10_plus_zero.c
@@ -0,0 +1,9 @@
+int f(int a) {
+    int ret = a + 0;
+    return ret;
+}
+
+int main() {
+return f(5);
+}
+
---
+++ tests/testfiles/arithmetics/13_mul_one_left.c
@@ -0,0 +1,9 @@
+int f(int a) {
+    int ret = 1 * a;
+    return ret;
+}
+
+int main() {
+return f(5);
+}
+
