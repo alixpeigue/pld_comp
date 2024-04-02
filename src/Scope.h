d714a455664fc1913b1e88273fee68f859e14f78
Alix Peigue <irobot.moi@gmail.com>
2024-04-02 15:27:24 +0200

    add doxygen config + add comments for Scope

--- src/Scope.h
+++ src/Scope.h
@@ -6,16 +6,43 @@
 #include <string>
 #include <unordered_map>
 
+/**
+ * @brief Classe représentant les types de variables en C
+ *
+ */
 class VarType {
 
 public:
     enum Type { VOID, INT };
 
     VarType() = default;
+
+    /**
+     * @brief Construit à partit de l'enum Type
+     *
+     * @param type
+     */
     VarType(Type type) : type(type){};
+
+    /**
+     * @brief Construit à partir du nom du type
+     *
+     * @param name nom c du type (ex : `int` ou `void`)
+     */
     VarType(const std::string &name);
+
+    /**
+     * @brief Conversion implicite en enum type
+     *
+     * @return Type
+     */
     operator Type() const { return type; }
 
+    /**
+     * @brief retourne la taille correspondant au type, exemple 4 pour int
+     *
+     * @return int
+     */
     int getSize() const { return sizes[type]; }
 
 protected:
@@ -25,15 +52,47 @@ protected:
 
 using Variable = std::pair<VarType, uint32_t>;
 
+/**
+ * @brief Scope représente la table des symboles des variables en prenant en
+ * compte leur portée
+ *
+ */
 class Scope {
 public:
     Scope(Scope &&scope)
         : parent(scope.parent), vars(std::move(scope.vars)), size(scope.size) {}
-    Scope(Scope *parent) : parent(parent), size(0){};
+    /**
+     * @brief Construit un scope vide avec `parent` comme parent. Le scope
+     parent est le scope dans lequel il est impriqué :
+     ```
+     a : {
+        b : {
 
+        }
+     }
+     ```
+     a est le parent de b
+     * @param parent le parent du scope
+     */
+    Scope(Scope *parent) : size(0), parent(parent){};
+
+    /**
+     * @brief Cherche une variable dans ce scope, si elle n'est pas trouvée dans
+     * ce scope, on cherche récursivement dans le parent.
+     *
+     * @param varName le nom de la variable
+     * @return la variable si elle é été trouvée, sinon la variante vide de
+     * std::optional
+     */
     std::optional<Variable> getVariable(const std::string &varName);
     uint32_t getSize();
 
+    /**
+     * @brief Ajoute une variable à ce scope
+     *
+     * @param name nom de la variable
+     * @param type type de la variable
+     */
     void addVariable(std::string name, VarType type) {
         size += 4;
         vars.insert(std::make_pair(name, std::make_pair(type, size)));
