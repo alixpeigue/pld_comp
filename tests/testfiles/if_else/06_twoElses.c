29cbf1bde6b7b4d9c58ea467f504c5c2dbe2792c
Alex4969 <alex.biaud@gmail.com>
2024-04-03 17:32:54 +0200

    add stupid test

---
+++ tests/testfiles/if_else/06_twoElses.c
@@ -0,0 +1,11 @@
+int main () {
+    int a = 2;
+    if (a == 2){
+        a = 2;
+    } else {
+        a = 3;
+    } else {
+        a = 4;
+    }
+    return a;
+}
---
+++ tests/testfiles/if_else/07_else_alone.c
@@ -0,0 +1,7 @@
+int main () {
+    int a = 2;
+    else {
+        a = 2;
+    }
+    return a;
+}
